17ea10e5e4211fa4d3d4efc6f9ac1e629393bbbd
Thuirox <anthony.bayet@hotmail.com>
2022-08-05 02:26:24 -0700

    + Method general to detect if user is on a mobile

--- script/index.js
+++ script/index.js
@@ -252,6 +252,35 @@ function main() {
 
     }
 
+    function isMobile(){
+        // Check if the user is usign a mobile device. https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+        let isMobile;
+
+        // First method working on all devices.
+        const toMatch = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i
+        ];
+        
+        isMobile = toMatch.some((toMatchItem) => {
+            return navigator.userAgent.match(toMatchItem);
+        });
+
+
+        // Seconde method. Not compatible with all devices. 
+        // Especially problematic for Iphone using Safari and WebView Android when the site opens up there.
+        // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/userAgentData
+        
+        // isMobile = navigator.userAgentData.mobile;
+
+        return isMobile;
+    }
+
     function setupManualControls(){
         // Init mouse and touch controls
         const controls = new OrbitControls(camera, canvas);
@@ -259,14 +288,13 @@ function main() {
         controls.enableZoom = false;
         controls.enableDamping = true;
 
-        // Check if the user is usign a mobile device. https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-        // const isMobile = navigator.userAgentData.mobile;
 
-        // if(isMobile){
-        //     controls.rotateSpeed = 0.7;
-        // } else {
-        //     controls.rotateSpeed = 0.5;
-        // }
+        if(isMobile()){
+            controls.rotateSpeed = 0.7;
+        } else {
+            controls.rotateSpeed = 0.5;
+        }
+
         controls.rotateSpeed = 0.5;
         // reverse control
         controls.rotateSpeed *= -1;
