f35f1bb067682a44ea73a32c9f5716c68b5f703c
Thuirox <anthony.bayet@hotmail.com>
2022-08-10 05:05:57 -0700

    + Button to start the experience - Translation

--- script/index.js
+++ script/index.js
@@ -111,16 +111,43 @@ function main() {
 
 main()
 
-// Loading screen animation
-setTimeout(()=>{
-    document.getElementById("loading-screen-break-strip__top").style.height = "100%";
-    document.getElementById("loading-screen-break-strip__bottom").style.height = "100%";
-    
-    setTimeout(()=>{
-        document.getElementById("loading-screen-left-panel").style.left = "-100%";
-        document.getElementById("loading-screen-right-panel").style.left = "calc(100% + 5px)";
-        document.getElementById("loading-screen").style.pointerEvents = "none";
-        
-    }, 1000)
 
-}, 1000)
+
+const startButton = document.getElementById("loading-screen-text-start");
+const startButtonContainer = document.getElementById("loading-screen-text-start-container");
+const loadingScreen = document.getElementById("loading-screen");
+
+setTimeout(()=>{
+    startButton.style.opacity = "0.8";
+
+    const startButtonAnimationTimeout = setTimeout(()=>{
+        startButton.classList.add("start-button-animation");
+
+    }, 2000)
+
+    setTimeout(()=>{
+        startButton.style.cursor = "pointer";
+
+        loadingScreen.onclick = () => {
+            clearTimeout(startButtonAnimationTimeout);
+            let currentOpacity = getComputedStyle(startButton).opacity;
+            startButton.style.opacity = currentOpacity;
+
+            startButton.classList.remove("start-button-animation");
+            
+            startButtonContainer.style.opacity = 0;
+        
+            // Loading screen animation
+            document.getElementById("loading-screen-break-strip__top").style.height = "100%";
+            document.getElementById("loading-screen-break-strip__bottom").style.height = "100%";
+            
+            setTimeout(()=>{
+                document.getElementById("loading-screen-left-panel").style.left = "-100%";
+                document.getElementById("loading-screen-right-panel").style.left = "calc(100% + 5px)";
+                loadingScreen.style.pointerEvents = "none";
+                
+            }, 1000)
+        }
+    }, 300);
+}, 700);
+
