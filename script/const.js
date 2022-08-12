b6116329915f64640fe43a1a87ac5e3935cb6e8b
Thuirox <anthony.bayet@hotmail.com>
2022-08-12 07:13:37 -0700

    # extract camera initial position to const file

--- script/const.js
+++ script/const.js
@@ -2,5 +2,11 @@ const debug_text = document.getElementById("debug_text");
 
 const gyroButton = document.getElementById("gyro-button");
 
+const cameraInitialAngle = {
+    x: 0,
+    y: 0,
+    z: 0.001
+}
 
-export { debug_text, gyroButton }
+
+export { debug_text, gyroButton, cameraInitialAngle }
