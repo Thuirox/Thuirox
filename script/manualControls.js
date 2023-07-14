769eb71c5a07d84240bfb8bf751e8f5941a876d1
justabayet <anthony.bayet@hotmail.com>
2023-07-14 17:53:35 +0200

    + OrbitControls lib

--- script/manualControls.js
+++ script/manualControls.js
@@ -1,4 +1,4 @@
-import { OrbitControls } from './libs/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { isMobile } from './utils.js';
 import { cameraInitialPosition } from './const.js';
 
